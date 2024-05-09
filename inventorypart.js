ea6d6c03c8b17275ef5da678ea91f49b6257e606
Dev-Juan-Medina-NutraApp <jmedina@nutrapp.com.co>
2024-05-09 13:48:02 -0500

    Correciones consumo endpoint /inventory

--- inventorypart.js
+++ inventorypart.js
@@ -5,7 +5,6 @@ const formInventory = document.getElementById('formPartNum');
 const formPlat = document.getElementById('formPlant');
 
 function inventoryPart() {
-        
     if(this.readyState === 4 && this.status === 200) {
         const data = JSON.parse(this.response);
         console.log(data);
@@ -28,7 +27,7 @@ function inventoryPart() {
         HTMLResponse.appendChild(productNamedesc);
 
         const emptyLine1 = document.createElement('div');
-        emptyLine1.style.marginBottom = "40px"; // Ajusta el valor según sea necesario
+        emptyLine1.style.marginBottom = "30px"; // Ajusta el valor según sea necesario
         HTMLResponse.appendChild(emptyLine1);
         const ul = document.createElement('ul');
         //const tpl = data.map((Inventory) => `<li>${Inventory.PartDesc}   ${Inventory.QtyOnHand}</li>`);
@@ -36,9 +35,9 @@ function inventoryPart() {
         inventoryList.forEach(item => {
             // Crear un elemento de lista (li) para cada objeto
             const li = document.createElement('li');
-      
+            console.log(item);
             // Crear el contenido del elemento li con las propiedades deseadas
-            const content = `EL lote ${item.LotNumber} tiene: ${item.QtyOnHand} Unidad(es) en la planta ${item.Plant}.`;
+            const content = `EL lote ${item.LotNumber} tiene: ${item.QtyOnHand} Unidad(es) en la planta ${item.Planta}.`;
             li.textContent = content;
       
             // Agregar el elemento li a la lista ul
@@ -46,42 +45,16 @@ function inventoryPart() {
           });
         // Agregar la lista ul al elemento HTMLResponse
         HTMLResponse.appendChild(ul);
+        const emptyLine2 = document.createElement('div');
+        emptyLine2.style.marginBottom = "15px"; // Ajusta el valor según sea necesario
+        HTMLResponse.appendChild(emptyLine2);
+        const separador = document.createElement('h3');
+        separador.textContent = ` - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - `;
+        HTMLResponse.appendChild(separador);
+        const emptyLine3 = document.createElement('div');
+        emptyLine3.style.marginBottom = "35px"; // Ajusta el valor según sea necesario
+        HTMLResponse.appendChild(emptyLine3);
         //HTMLResponse.innerHTML = `<p>${data}</p>`
-    }
-}
-
-function inventoryPlant() {
-        
-    if(this.readyState === 4 && this.status === 200) {
-        const data = JSON.parse(this.response);
-        console.log(data);
-        
-        const HTMLResponse = document.querySelector("#appPlant");
-        const inventoryList = data.result.Inventory;
-        const paragraph = document.createElement('p');
-        
-        paragraph.textContent = JSON.stringify(data.message);
-        //const productName = data.result.Inventory[0].Plant;
-
-        const productNameElement = document.createElement('h2');
-        //productNameElement.textContent = `La Planta: ${productName}`;
-        HTMLResponse.appendChild(productNameElement);
-        const productNamedesc = document.createElement('h3');
-        productNamedesc.textContent = `cuenta con el siguiente inventario en EPICOR:`;
-        HTMLResponse.appendChild(productNamedesc);
-
-        const emptyLine1 = document.createElement('div');
-        HTMLResponse.appendChild(emptyLine1);
-        const ul = document.createElement('ul');
-        inventoryList.forEach(item => {
-            const li = document.createElement('li');
-      
-            const content = `EL lote ${item.LotNumber} tiene: ${item.QtyOnHand} Unidad(es) en la planta ${item.Plant}.`;
-            li.textContent = content;
-      
-            ul.appendChild(li);
-          });
-        HTMLResponse.appendChild(ul);
     }
 }
 
@@ -89,9 +62,12 @@ formInventory.addEventListener('submit', function(event) {
     event.preventDefault(); // Evitar el envío del formulario
     const partNumInput = document.getElementById('partNum');
     const partNum = partNumInput.value;
+    const partPlantInput = document.getElementById('partPlant');
+    const partPlant = partPlantInput.value;
 
     const datos = {
-        "PartNum": partNum
+        "PartNum": partNum,
+        "Plant" : partPlant
       };
       console.log(datos);
       xhr.open('POST', `${API_URL}/InventoryPart`);
@@ -101,20 +77,3 @@ formInventory.addEventListener('submit', function(event) {
       xhr.addEventListener("load", inventoryPart);
       xhr.send(JSON.stringify(datos));
   });
-
-formPlant.addEventListener('submit', function(event) {
-event.preventDefault(); // Evitar el envío del formulario
-const PlantInput = document.getElementById('Plant');
-const Plant = PlantInput.value;
-
-const datos = {
-    "PartNum": Plant
-    };
-    console.log(datos);
-    xhr.open('POST', `${API_URL}/InventoryPart`);
-    xhr.setRequestHeader('x-api-key', 'daJJpGNt9IvBlSLfpl0oxsWx1ngwcte8fit9lknG8Y9wA');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa('JMEDINA:Nutra2023*#'));
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.addEventListener("load", inventoryPlant);
-    xhr.send(JSON.stringify(datos));
-});
